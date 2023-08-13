f8b6bbcfe3897498c28ab12052982abecd3dad24
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-08-14 02:59:54 +0530

    Time: 754 ms (76.30%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -8,4 +8,5 @@ FROM Employee e1
 JOIN Employee e2 ON e1.managerId = e2.id
 WHERE e1.salary > e2.salary;
 
-
+# SELECT e1.name  as Employeefrom Employee e1 , Employee e2 
+# WHERE e1.managerId = e2.id and e1.salary > e2.salary;
