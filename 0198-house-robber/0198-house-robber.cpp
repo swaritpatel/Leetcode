8193d79f7a0e605c8ac64ab9882dc19416211acd
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-09-08 10:48:11 +0530

    Time: 2 ms (40.56%), Space: 7.8 MB (51.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,15 +4,21 @@ public:
         int n = nums.size();
         vector<int>dp(n,0);
         dp[0] = nums[0];
+         
+        int prev=nums[0],prev2 = 0,curr;
         
         for(int i = 1; i < n; i++){
             int pick = nums[i];
             if(i >1)
-             pick += dp[i-2]; 
-            int nonpick = 0 + dp[i-1];
-            dp[i] = max(pick,nonpick);
+             pick += prev2; 
+            int nonpick = 0 + prev;
+            
+            curr = max(pick,nonpick);
+            prev2 = prev;
+            prev = curr;
+            
         }
-        return dp[n-1];
+        return prev;
     
         
         
