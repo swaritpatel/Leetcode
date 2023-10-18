fe46b99aa494df5426e5ece1e690d9c9568a8650
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-10-18 20:16:58 +0530

    Time: 2564 ms (84.68%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,4 @@
+SELECT salesperson.name
+FROM orders o JOIN company c ON (o.com_id = c.com_id AND c.name = 'RED')
+RIGHT JOIN salesperson ON salesperson.sales_id = o.sales_id
+WHERE o.sales_id IS NULL
