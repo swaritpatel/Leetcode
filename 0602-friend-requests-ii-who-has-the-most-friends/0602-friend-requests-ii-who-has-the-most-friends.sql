1fe41dcfe2d0328afe4541c301426855afd52f53
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-10-18 20:10:20 +0530

    Time: 836 ms (12.47%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+WITH friends AS (SELECT requester_id id
+FROM RequestAccepted
+UNION ALL
+SELECT accepter_id
+FROM RequestAccepted)
+
+SELECT id, COUNT(id) num
+FROM friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
