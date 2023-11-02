2bf79c8dac945c7597f8d944f55801183f9d2279
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 18:46:03 +0530

    Time: 821 ms (61.59%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,7 @@
-SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+# SELECT w1.id
+# FROM Weather w1, Weather w2
+# WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+
+
+select w1.id from Weather w1, Weather w2 
+where DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
