ba74d446377612b98a53e4d73b876c8464dfa6f1
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 18:34:51 +0530

    Time: 2392 ms (22.08%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,5 +1,31 @@
-with cte as (
-SELECT*,DENSE_RANK() over(PARTITION BY t.Department ORDER BY t.Salary desc)  as "rank" from
-(SELECT  d1.name as "Department",e1.name "Employee",e1.salary  as "Salary" from employee e1 join department d1 on e1.`departmentId` = d1.id) t
-)
-SELECT c.Department,c.Employee ,c.Salary from cte c where c.rank<=3;
+# SELECT DEPT.name AS Department, EMP.name AS Employee, EMP.salary 
+
+# AS Salary FROM Department DEPT JOIN Employee EMP ON 
+
+# EMP.DepartmentId=DEPT.id WHERE 3 > (SELECT COUNT(DISTINCT EMP1.salary)
+ 
+# FROM Employee EMP1 WHERE EMP1.salary > EMP.salary AND 
+
+# EMP.departmentId = EMP1.departmentId)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT d.name as Department, e.name as Employee , e.salary as Salary FROM Department d JOIN Employee e
+ON e.DepartmentId = d.id WHERE 3 > (SELECT COUNT(Distinct e1.salary) FROM Employee e1 WHERE e1.salary > e.salary and e1.departmentId = e.departmentId)
+
+
