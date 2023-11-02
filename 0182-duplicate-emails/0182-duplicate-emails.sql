0137a51557bc484e70cb996855bc0dd00f294404
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 13:20:38 +0530

    Time: 618 ms (90.07%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -22,4 +22,7 @@
 # SELECT email from Person group by email having count(email) >1;
 
 
-SELECT distinct(p1.Email) from Person p1, Person p2 where p1.id <> p2.id and p1.email = p2.email;
+# SELECT distinct(p1.Email) from Person p1, Person p2 where p1.id <> p2.id and p1.email = p2.email;
+
+SELECT distinct(p1.email) from person p1 JOIN person p2
+ON p1.email = p2.email and  p1.id <> p2.id ;
