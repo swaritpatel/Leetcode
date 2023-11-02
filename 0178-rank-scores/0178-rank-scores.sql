94f34f8d0bb39c0a51392c7a7261a017074940e2
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 11:15:43 +0530

    Time: 690 ms (30.93%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,10 +1,15 @@
-SELECT _score.score,
-    (
-        SELECT count(DISTINCT (distinct_score.score)) + 1
-        FROM Scores distinct_score
-        WHERE distinct_score.score > _score.score
-        ) 
-        AS 'rank'
+# SELECT _score.score,
+#     (
+#         SELECT count(DISTINCT (distinct_score.score)) + 1
+#         FROM Scores distinct_score
+#         WHERE distinct_score.score > _score.score
+#         ) 
+#         AS 'rank'
         
-FROM Scores _score
-ORDER BY _score.score DESC
+# FROM Scores _score
+# ORDER BY _score.score DESC
+
+
+select score, dense_rank() over(order by score desc) as 'rank' from Scores;
+
+# Select score, dense_rank() over (Order By score DESC) as 'rank' from Scores ;
