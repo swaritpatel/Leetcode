7d4cc5c2a877021b603a04c0469eca835afce8ae
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 12:58:59 +0530

    Time: 605 ms (94.79%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,12 +1,14 @@
-# Write your MySQL query statement below
-# SELECT e1.name from Employee e1
-# JOIN Employee e2 ON e1.managerId > e2.id
+# # Write your MySQL query statement below
+# # SELECT e1.name from Employee e1
+# # JOIN Employee e2 ON e1.managerId = e2.id
+# # WHERE e1.salary > e2.salary;
+
+# SELECT e1.name as Employee
+# FROM Employee e1
+# JOIN Employee e2 ON e1.managerId = e2.id
 # WHERE e1.salary > e2.salary;
 
-SELECT e1.name as Employee
-FROM Employee e1
-JOIN Employee e2 ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary;
+# # SELECT e1.name  as Employee from Employee e1 , Employee e2 
+# # WHERE e1.managerId = e2.id and e1.salary > e2.salary;
 
-# SELECT e1.name  as Employeefrom Employee e1 , Employee e2 
-# WHERE e1.managerId = e2.id and e1.salary > e2.salary;
+SELECT e1.name as Employee from Employee e1 , Employee e2 where e1.managerId = e2.id and e1.salary > e2.salary;
