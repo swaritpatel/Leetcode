02ecae9d20fd168fc46528642827bc034b5e8969
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-02 14:21:39 +0530

    Time: 1112 ms (91.56%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,14 +1,9 @@
-WITH cte AS (
-    SELECT d.name AS department,
-           e.name AS employee,
-           e.salary,
-           max(e.salary) OVER (PARTITION BY d.id) AS max_salary
-      FROM Employee e
-          JOIN Department d
-              ON e.departmentId = d.id
-)
-SELECT department,
-       employee,
-       salary
-  FROM cte
- WHERE salary = max_salary;
+SELECT DEPT.name AS Department, EMP.name AS Employee, EMP.salary AS 
+
+Salary FROM Department DEPT, Employee EMP WHERE
+
+EMP.departmentId = DEPT.id AND (EMP.departmentId, salary) IN 
+
+(SELECT departmentId, MAX(salary) FROM Employee GROUP BY 
+
+departmentId);
