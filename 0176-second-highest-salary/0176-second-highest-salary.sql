bcd9bdce4e5e80ac96fc1f34252de57714accf90
Swarit Patel <78478463+swaritpatel@users.noreply.github.com>
2023-11-10 14:44:46 +0530

    Time: 407 ms (87.98%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -9,5 +9,12 @@
 
 # select t1.salary as SecondHighestSalary from Employee as t1 where 1= (select (count(distinct(salary))) from Employee as t2 where t1.salary>t2.salary)/
 
-select max(salary) as SecondHighestSalary
-FROM employee where salary <> (Select max(salary) from employee)
+# select max(salary) as SecondHighestSalary
+# FROM employee where salary <> (Select max(salary) from employee)
+
+Select(SELECT distinct e1.salary  from Employee e1 
+ order by salary desc
+Limit  1 offset 1) as SecondHighestSalary;
+
+# select (select distinct salary from employee 
+#         order by salary desc limit 1 offset 1) as SecondHighestSalary;
